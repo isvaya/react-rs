c7b6243c47ffaf1bea8c18a933b17d8688ba99cb
Isvaya <tasyana.pulatova@gmail.com>
2025-07-21 00:01:53 +0500

    feat: add test for error boundary

---
+++ src/result/SearchResults.test.tsx
@@ -0,0 +1,38 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { SearchResult } from './SearchResults';
+
+describe('SearchResult', () => {
+  it('shows spinner when loading=true', () => {
+    const { container } = render(<SearchResult history={[]} loading={true} />);
+    expect(container.querySelector('.spinner')).toBeInTheDocument();
+  });
+
+  it('renders title and strokes of table from history', () => {
+    const history = [
+      { name: 'pickachu', description: 'electric mouse' },
+      { name: 'eevee', description: 'evolution Pokemon' },
+    ];
+    render(<SearchResult history={history} loading={false} />);
+
+    expect(screen.getByText('RESULTS:')).toBeInTheDocument();
+
+    history.forEach(({ name, description }) => {
+      expect(screen.getByText(name)).toBeInTheDocument();
+      expect(screen.getByText(description)).toBeInTheDocument();
+    });
+  });
+
+  it('render correct quantity of strokes in <tbody>', () => {
+    const history = [
+      { name: 'a', description: '1' },
+      { name: 'b', description: '2' },
+      { name: 'c', description: '3' },
+    ];
+    const { container } = render(
+      <SearchResult history={history} loading={false} />
+    );
+    const rows = container.querySelectorAll('tbody tr');
+    expect(rows).toHaveLength(history.length);
+  });
+});
