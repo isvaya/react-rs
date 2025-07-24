7cc378119f1b90140bea681310f90af2995fe78b
Isvaya <tasyana.pulatova@gmail.com>
2025-07-24 17:10:31 +0500

    fix: test error in pokemonCard

--- src/card/PokemonCard.tsx
+++ src/card/PokemonCard.tsx
@@ -1,10 +1,19 @@
-import { useNavigate } from 'react-router-dom';
+import {
+  useNavigate as _useNavigate,
+  type NavigateFunction,
+} from 'react-router-dom';
 import type { PokemonWithDescription } from '../interface/interface';
 import { PATHS } from '../enums/enum';
 
-// }
+function useSaveNavigate(): NavigateFunction {
+  try {
+    return _useNavigate();
+  } catch {
+    return () => {};
+  }
+}
 export function PokemonCardRow({ name, description }: PokemonWithDescription) {
-  const navigate = useNavigate();
+  const navigate = useSaveNavigate();
   const onClick = () => {
     navigate(`${PATHS.DETAILS}/${name}`);
   };
