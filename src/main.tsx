ba985dc17ef15fe2c9ac9df62b7b600d71c56594
Isvaya <tasyana.pulatova@gmail.com>
2025-08-05 12:10:33 +0500

    feat: create and provide QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,25 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/Router';
 import { ThemeProvider } from './context/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Root element not found');
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
